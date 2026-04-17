21a9d9f28b4d6a88f9b45761f2c3b2eaecae627f
Aluno SENAI <ruhan.nes@gmail.com>
2026-04-16 22:19:47 -0300

    Testes javascript

---
+++ 01-Javascript/testes.js
@@ -0,0 +1,23 @@
+// alert("Olá mundo!")
+// alert("SEGUNDA MENSAGEM")
+
+let nome 
+
+nome = "Nome"
+
+// alert ("Nobari: " + nome)
+
+let idade = 32
+// alert("Idade: " + idade)
+
+// alert("Nome: " + nome + "\nIdade: " + idade)
+
+// nome = prompt ("Digite seu nome: ")
+// alert("Nome digitado: " + nome)
+
+
+let nota1
+nota1 = prompt ("Digite o valor da primeira nota: ")
+nota1 = Number(nota1)
+nota1 = nota1 + 1
+alert("A primeira nota é: " + nota1)
